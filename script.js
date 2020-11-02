42a1d81573f0f53e6f39a4587cc4f671f0405601
tolunchuk <playgametolun@mail.ru>
2020-11-02 21:34:09 +1000

    123
    123

--- script.js
+++ script.js
@@ -3,19 +3,6 @@
 	//document.querySelector(``).addEventListener(`click`,function() {
 		
 	//});
-	var timer1 = 46;
-	var timer2 = 60;
-	
-	setInterval(function() {
-		if(timer2 == 0) {
-		timer1 = timer1 - 1;
-		timer2 = 60;
-		}
-		timer2 = timer2 - 1;
-		
-		tablo.innerHTML = "Скидки Продлятся Ещё: " + timer1 + ":" + timer2;
-	},1000);
-	
 	document.querySelector(`body`).onkeypress = function(event) {
 		
 	}
